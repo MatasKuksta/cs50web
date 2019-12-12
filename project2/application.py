9c81c5d8e7e71af24e42e292904d027a66c74103
MatasKuksta <matkuksta@gmail.com>
2019-12-12 18:52:20 +0200

    lol

--- project2/application.py
+++ project2/application.py
@@ -13,10 +13,26 @@ def index():
     return render_template("index.html")
 
 
-@socketio.on('my event')
+@socketio.on('sendmsg')
 def handle_my_custom_event(json, methods=['GET', 'POST']):
     print('recieved my event: '+ str(json))
     socketio.emit('my response', json)
+
+
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    send(username + ' has entered the room.', room=room)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send(username + ' has left the room.', room=room)
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
