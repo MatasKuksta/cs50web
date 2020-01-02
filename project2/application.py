ea953a2cf349a35b5a1ab589ce83797936d36640
MatasKuksta <matkuksta@gmail.com>
2020-01-02 20:50:18 +0200

    czxczx

--- project2/application.py
+++ project2/application.py
@@ -4,8 +4,7 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit, join_room, leave_room, send
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = 'vnkdjnfjknfl1232#'
-# os.getenv("SECRET_KEY")
+os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
 @app.route("/")
