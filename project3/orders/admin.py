54c1768414e2fc97f7e28bca3cccd911fc1863fc
MatasKuksta <matkuksta@gmail.com>
2020-02-04 17:29:25 +0200

    All models exist

--- project3/orders/admin.py
+++ project3/orders/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Toppings, Subs, Pasta, Salads, Platters
+from .models import Toppings, Subs, Pasta, Salads, Platters, Kind, Size, Pizza
 
 # Register your models here.
 admin.site.register(Toppings)
@@ -7,3 +7,6 @@ admin.site.register(Subs)
 admin.site.register(Pasta)
 admin.site.register(Salads)
 admin.site.register(Platters)
+admin.site.register(Kind)
+admin.site.register(Size)
+admin.site.register(Pizza)
--- project3/orders/models.py
+++ project3/orders/models.py
@@ -37,3 +37,24 @@ class Platters(models.Model):
 
     def __str__(self):
         return f"{self.platter},{self.priceSmall},{self.priceLarge}"
+
+class Kind(models.Model):
+    kind = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.kind}"
+
+class Size(models.Model):
+    size = models.CharField(max_length=5)
+
+    def __str__(self):
+        return f"{self.size}"
+
+class Pizza(models.Model):
+    kind = models.ForeignKey(Kind, on_delete=models.CASCADE)
+    name = models.TextField(max_length=11, default="Default")
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    price = models.FloatField(default=10)
+
+    def __str__(self):
+        return f"{self.kind},{self.name}, {self.size}"
