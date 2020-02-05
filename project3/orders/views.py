de04dc909fb7555e70c36e538e66cef659312856
MatasKuksta <43648422+MatasKuksta@users.noreply.github.com>
2020-02-05 20:01:14 +0200

    registr

--- project3/orders/views.py
+++ project3/orders/views.py
@@ -1,6 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        email = request.POST["email"]
+        firstname = request.POST["firstname"]
+        lastname = request.POST["lastname"]
+        if User.objects.filter(username=username).exists():
+            return render(request, "orders/register.html", {"message": "Username already exists"})
+        else:
+            user = User.objects.create_user(username, email, password)
+            user.first_name = firstname
+            user.last_name = lastname
+            user.save()
+            return render(request, "orders/login.html")
