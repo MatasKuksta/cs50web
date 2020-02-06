b7a8e3aefa6efad6ea8bf81f9dddff077c688c2c
MatasKuksta <matkuksta@gmail.com>
2020-02-06 18:43:21 +0200

    LOGIN AND LOGOUT WORK! Sessions dont exist tho

--- project3/orders/views.py
+++ project3/orders/views.py
@@ -1,5 +1,7 @@
-from django.http                    import HttpResponse
-from django.contrib.auth            import login, authenticate, logout
+from django.http                    import HttpResponse, HttpResponseRedirect
+from django.contrib.auth            import authenticate
+from django.contrib.auth            import login as auth_login
+from django.contrib.auth            import logout as auth_logout
 from django.shortcuts               import render, redirect
 from django.contrib.auth.models     import User
 from django.urls                    import reverse
@@ -26,3 +28,20 @@ def register(request):
             return render(request, "login.html")
     else:
         return render(request, "register.html")
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return redirect(request, "login.html", {"message": "username and password do not match"})
+    else:
+        return render(request, "login.html")
+
+def logout(request):
+    auth_logout(request)
+    return render(request, "login.html", {"message": "logged out user"})
